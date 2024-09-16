ba9b87f953806b14ba843e45423786a07254a1fd
stua1125 <stua92@gmail.com>
2024-09-16 23:41:16 +0900

    Initial commit

---
+++ src/main/java/spring_security_oauth/spring_security_oauth/SpringSecurityOauthApplication.java
@@ -0,0 +1,13 @@
+package spring_security_oauth.spring_security_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_security_oauth/spring_security_oauth/SpringSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_security_oauth.spring_security_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
